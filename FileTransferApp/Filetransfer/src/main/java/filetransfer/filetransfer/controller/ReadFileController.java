3f6ff2dad16887bd69206c456d6a0ff0f857ddef
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-02-17 10:53:37 -0800

    commi

--- FileTransferApp/Filetransfer/src/main/java/filetransfer/filetransfer/controller/ReadFileController.java
+++ FileTransferApp/Filetransfer/src/main/java/filetransfer/filetransfer/controller/ReadFileController.java
@@ -2,41 +2,35 @@ package filetransfer.filetransfer.controller;
 
 import filetransfer.filetransfer.dtos.request.FileReaderAndWriterRequest;
 import filetransfer.filetransfer.dtos.response.FileReaderAndWriterResponse;
-import filetransfer.filetransfer.dtos.response.FileReaderResponse;
 import filetransfer.filetransfer.service.FileReader;
 import filetransfer.filetransfer.service.FileReaderAndWriter;
-import filetransfer.filetransfer.service.FileReaderAndWriterImpl;
 import filetransfer.filetransfer.service.FileReaderImpl;
+import filetransfer.filetransfer.service.FileReaderAndWriterImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
-public class ReadFileContoller {
+public class ReadFileController {
     private FileReader fileReader;
     private FileReaderAndWriter fileReaderAndWriter;
-    public ReadFileContoller() {
+
+    public ReadFileController() {
         this.fileReader = new FileReaderImpl();
         this.fileReaderAndWriter = new FileReaderAndWriterImpl();
     }
 
     @GetMapping("/{fileLocation}")
-    public ResponseEntity<?> readFile( String fileLocation) {
+    public ResponseEntity<?> readFile(@PathVariable String fileLocation) {
         String fileReaderResponse = fileReader.readFile(fileLocation);
         return ResponseEntity.status(HttpStatus.FOUND).body(fileReaderResponse);
     }
 
     @PostMapping("/readAndWriteFile")
-    public ResponseEntity<?> readAndWriteFile(FileReaderAndWriterRequest fileReaderAndWriterRequest) throws IOException {
-        FileReaderAndWriterResponse  fileReaderAndWriterResponse = fileReaderAndWriter.readsAndWriteToFile(fileReaderAndWriterRequest);
+    public ResponseEntity<?> readAndWriteFile(@RequestBody FileReaderAndWriterRequest fileReaderAndWriterRequest) throws IOException {
+        FileReaderAndWriterResponse fileReaderAndWriterResponse = fileReaderAndWriter.readsAndWriteToFile(fileReaderAndWriterRequest);
         return ResponseEntity.status(HttpStatus.FOUND).body(fileReaderAndWriterResponse);
     }
-
-
-
 }
--- FileTransferApp/Filetransfer/src/test/java/FileReaderAndWriterImplTest.java
+++ FileTransferApp/Filetransfer/src/test/java/FileReaderAndWriterImplTest.java
@@ -6,6 +6,7 @@ import filetransfer.filetransfer.service.FileReaderImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,7 +29,7 @@ class FileReaderAndWriterImplTest {
         fileReaderAndWriterRequest.setFilePath("/home/notdotun/Desktop/GITHUB/transfer2/src/main/java/filetransfer/transfer2/filetransfer/utils/");
         fileReaderAndWriterRequest.setLocalPort("8080/readFile?fileLocation=");
         fileReaderAndWriterRequest.setIpAddress("http://127.0.0.1:");
-        fileReaderAndWriterRequest.setFileWriterLocation("/home/notdotun/Desktop/GITHUB/CinemaBookingApp/Filetransfer/src/main/java/filetransfer/filetransfer/utils/result.json");
+        fileReaderAndWriterRequest.setFileWriterLocation("/home/notdotun/Desktop/GITHUB/FileTransferApp/Filetransfer/src/main/java/filetransfer/filetransfer/utils/result.json");
     }
     @Test
     void testThatFileCanBeReadAndResultWritten() throws IOException {
